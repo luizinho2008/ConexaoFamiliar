eebd567c530195a30a0d0bb98051e0aadaf7dd34
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2025-05-18 13:03:47 -0300

    Arrumando erro do cadastro

--- frontend/src/components/Cadastro/Cadastro.jsx
+++ frontend/src/components/Cadastro/Cadastro.jsx
@@ -55,7 +55,7 @@ const Cadastro = () => {
     
         if (!errors.nome && !errors.cpf && !errors.idade) {
             try {
-                const response = await axios.post("https://conexaofamiliar.onrender.com/", {
+                const response = await axios.post("https://conexaofamiliar.onrender.com/cadPost", {
                     nome,
                     cpf,
                     idade
